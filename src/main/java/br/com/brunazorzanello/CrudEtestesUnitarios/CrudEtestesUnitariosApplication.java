90cd9bd7026f1e54aa4ac95e448dd82ae7644bb0
ZorzanelloBruna <brunazorzanello@gmail.com>
2023-01-18 16:25:23 -0300

    projeto iniciado

---
+++ src/main/java/br/com/brunazorzanello/CrudEtestesUnitarios/CrudEtestesUnitariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunazorzanello.CrudEtestesUnitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEtestesUnitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEtestesUnitariosApplication.class, args);
+	}
+
+}
