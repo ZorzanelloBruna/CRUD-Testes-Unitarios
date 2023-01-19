66319daea5b3f886ecb0ad6f42f4e5f945fdc544
ZorzanelloBruna <brunazorzanello@gmail.com>
2023-01-19 09:39:16 -0300

    Tratamento de excessão personalizado

--- src/main/java/br/com/brunazorzanello/CrudEtestesUnitarios/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/brunazorzanello/CrudEtestesUnitarios/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.brunazorzanello.CrudEtestesUnitarios.services.impl;
 import br.com.brunazorzanello.CrudEtestesUnitarios.domain.User;
 import br.com.brunazorzanello.CrudEtestesUnitarios.repositorys.UserRepository;
 import br.com.brunazorzanello.CrudEtestesUnitarios.services.UserService;
+import br.com.brunazorzanello.CrudEtestesUnitarios.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> user = repository.findById(id);
-        return user.orElse(null);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado no banco de dados!"));
     }
 }
