d915f7306811e5635518c95182b69ce0b7139002
ZorzanelloBruna <brunazorzanello@gmail.com>
2023-01-19 14:11:20 -0300

    teste para a classe principal

--- src/test/java/br/com/brunazorzanello/CrudEtestesUnitarios/CrudEtestesUnitariosApplicationTests.java
+++ src/test/java/br/com/brunazorzanello/CrudEtestesUnitarios/CrudEtestesUnitariosApplicationTests.java
@@ -3,11 +3,13 @@ package br.com.brunazorzanello.CrudEtestesUnitarios;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class CrudEtestesUnitariosApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		CrudEtestesUnitariosApplication.main(new String[] {});
 	}
 
 }
